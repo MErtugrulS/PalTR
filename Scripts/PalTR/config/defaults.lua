857308f452a28cdffa14453c153371837188d92d
MErtugrulS <mustafaertugrulsaruhan@gmail.com>
2026-08-11 20:41:09 +0300

    Faz-05 ayakli tabelayi Klan Bayragi olarak dogrula

--- Scripts/PalTR/config/defaults.lua
+++ Scripts/PalTR/config/defaults.lua
@@ -31,7 +31,9 @@ return {
         max_outposts_per_clan = 10,
         world_units_per_meter = 100,
         flag_interaction_radius_meters = 20,
-        conquest_flag_actor_class_tokens = {},
+        conquest_flag_actor_class_tokens = {
+            "BP_BuildObject_Signboard_C"
+        },
         flag_candidate_actor_class_tokens = {
             "BP_BuildObject_Signboard"
         },
